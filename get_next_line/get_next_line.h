76a527852b5091bfbf6ea82f0bbcc0b7b225121e
Stevocado <97487863+StevenLikesToCode@users.noreply.github.com>
2023-01-02 16:20:09 +1030

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suchen <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 17:33:02 by suchen            #+#    #+#             */
+/*   Updated: 2022/10/15 17:33:15 by suchen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+
+#endif
